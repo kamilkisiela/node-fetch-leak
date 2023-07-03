5b2d95c30d15b5993bae91e41fcc2a6f5fae7d40
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-07-03 16:10:17 +0200

    lfg

--- k6.js
+++ k6.js
@@ -1,5 +1,7 @@
 import http from "k6/http";
 
+const endpoint = `http://${__ENV.ENDPOINT_HOSTNAME || "localhost"}:3000`;
+
 export default function () {
-  http.get("http://localhost:3000");
+  http.get(endpoint);
 }
